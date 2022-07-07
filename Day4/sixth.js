4ea6e589882834724d3517c99e50ace1d555f4e1
vishnu kumar <81406208+Vishnu3522@users.noreply.github.com>
2022-07-07 16:47:09 +0530

    Update sixth.js

--- Day4/sixth.js
+++ Day4/sixth.js
@@ -1,9 +1,13 @@
-const checkGrade = (marks, examType) =>{
-    if((examType === "final" && marks >=90) || (marks >= 89 && examType != "final" ))
-        return true;
-    else return false;
+function checkGrade(marks, examType) {
+  if (marks >= 90) {
+    grade = "A+";
+    if (examType === "final" && grade === "A+") {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
 
-};
-
-console.log(checkGrade(92, "mid"));
-console.log(checkGrade(85,"final"));
+console.log(checkGrade(92, "mtt"));
+console.log(checkGrade(95, "final"));
