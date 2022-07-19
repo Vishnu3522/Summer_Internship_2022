8422ad398d5bf0630a973c4b8ec287211a426391
Vishnu <vishnukumar73680@gmail.com>
2022-07-19 10:21:55 +0530

    code

---
+++ DAy11/gitsrc/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Showprofile from './Components/Showprofile';
+
+function App() {
+  return (
+    <div className="App">
+      <Showprofile/>
+    </div>
+  );
+}
+
+export default App;
